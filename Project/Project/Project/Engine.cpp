2775034442a5470b31ab20f952bccba5ebee5f6a
Picopic <pico@live.se>
2013-12-21 16:13:30 +0100

    SDL, Engine
    Lade till SDL library och gjorde Engine.h/.cpp och Log.h/.cpp

---
+++ Project/Project/Project/Engine.cpp
@@ -0,0 +1,68 @@
+#include <SDL.h>
+#pragma comment(lib,"sdl2.lib")
+#pragma comment(lib,"sdl2main.lib")
+
+#include "Engine.h"
+
+Engine::Engine() : m_log("log.txt") {
+	m_window = nullptr;
+
+	m_running = false;
+	m_width = 0;
+	m_height = 0;
+	m_deltatime = 0.01f;
+	m_ticks = SDL_GetTicks();
+};
+
+Engine::~Engine() {
+};
+
+bool Engine::Initialize() {
+	m_width = 1024;
+	m_height = 640;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	m_window = SDL_CreateWindow("DuckHunt", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		m_width, m_height,
+		SDL_WINDOW_OPENGL);
+	if(m_window == nullptr) { return false; };
+
+	m_running = true;
+
+	return true;
+};
+
+void Engine::Run() {
+	while(m_running) {
+		UpdateDeltatime();
+		UpdateEvents();
+		};
+		SDL_Delay(10);
+};
+
+void Engine::Cleanup() {
+	if(m_window != nullptr) {
+		SDL_DestroyWindow(m_window);
+		m_window = nullptr;
+	};
+};
+
+
+void Engine::UpdateDeltatime() {
+	unsigned int ticks = SDL_GetTicks();
+	unsigned int delta = ticks - m_ticks;
+	m_ticks = ticks;
+	m_deltatime = (float)delta * 0.001f;
+	if(m_deltatime > 0.1f) {
+		m_deltatime = 0.1f;
+	};
+};
+
+void Engine::UpdateEvents() {
+	SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+		if(event.type == SDL_QUIT) {
+			m_running = false;
+		}
+	};
+};
--- Project/Project/Project/main.cpp
+++ Project/Project/Project/main.cpp
@@ -1,6 +1,11 @@
+#include "Engine.h"
 
-
-int main(int argv, char *argc[]){
+int main(int argc, char *argv[]) {
+	Engine engine;
+	if(engine.Initialize()) {
+		engine.Run();
+	};
+	engine.Cleanup();
 
 	return 0;
-}
+};
