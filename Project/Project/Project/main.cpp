cbaa0707252ad70ee8ebc02b383ec23d07bd66fc
Robin K <robin.karlsson@bredband2.com>
2013-12-23 17:38:04 +0100

    Aditional Libs added
    Added a bunch of libraries neccesary to run the program properly,
    updated the project properties and added stuff to main.ccp aswell

--- Project/Project/Project/main.cpp
+++ Project/Project/Project/main.cpp
@@ -1,4 +1,10 @@
-
+#include <SDL.h>
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "sdl2main.lib")
+#include <SDL_image.h>
+#pragma comment(lib, "sdl2_image.lib")
+#include <SDL_mixer.h>
+#pragma comment(lib, "sdl2_mixer.lib")
 
 int main(int argv, char *argc[]){
 
