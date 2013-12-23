a28163775a98568ab54ddf00b8f9c00775bda68c
Robin K <robin.karlsson@bredband2.com>
2013-12-23 17:35:46 +0100

    Sprite Class + SpriteManager
    Wrote code for sprite/sprite manager

--- Project/Project/Project/Sprite.h
+++ Project/Project/Project/Sprite.h
@@ -1,9 +1,20 @@
-
 #pragma once
 
-class Sprite{
-public:
-	Sprite();
-	~Sprite();
+struct SDL_Texture;
+struct SDL_Surface;
 
+class Sprite{
+	friend class DrawManager;
+	friend class SpriteManager;
+
+public:
+	Sprite(SDL_Surface *surface, SDL_Texture *texture, int x, int y, int width, int height);
+
+protected:
+	SDL_Surface *m_surface;
+	SDL_Texture *m_texture;
+	int m_x;
+	int m_y;
+	int m_height;
+	int m_width;
 };
--- Project/Project/Project/SpriteManager.h
+++ Project/Project/Project/SpriteManager.h
@@ -1,7 +1,30 @@
-
 #pragma once
+#include <map>
+//#include "AnimatedSprite.h"					//code for if/when animation is added
+#include <string>
+
+class Sprite;
+class DrawManager;
 
 class SpriteManager{
-	SpriteManager();
+	struct Pair{
+		struct SDL_Surface *surface;
+		struct SDL_Texture *texture;
+	};
 
+public:
+	SpriteManager(DrawManager *draw_manager);
+	~SpriteManager();
+
+	bool Initialize(const std::string &directory);
+	void cleanup();
+	bool LoadImage(std::string &filename);
+	virtual Sprite* Load(const std::string &filename, int x, int y, int width, int height);
+	//AnimatedSprite* Load(const std::string &filename);					//code for if/when animation is added
+private:
+	std::string m_directory;
+	std::string m_filename;
+	//AnimatedSprite::Frame m_frame;					//code for if/when animation is added
+	std::map<std::string,Pair> m_sprites;
+	DrawManager *m_draw_manager;
 };
