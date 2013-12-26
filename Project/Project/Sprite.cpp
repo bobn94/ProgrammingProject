2eb7871eb7c7c7f671f4340bd9b510b64c437548
Robin K <robin.karlsson@bredband2.com>
2013-12-26 15:01:32 +0100

    Gjorde om Hela projectet
    Tog bort allt, gjorde ett nytt project och lade in allt igen

---
+++ Project/Project/Sprite.cpp
@@ -0,0 +1,12 @@
+
+#include "Sprite.h"
+#include "SpriteManager.h"
+
+Sprite::Sprite(SDL_Surface *surface, SDL_Texture *texture, int x, int y, int width, int height){
+	m_surface = surface;
+	m_texture = texture;
+	m_x = x;
+	m_y = y;
+	m_width = width;
+	m_height = height;
+}
